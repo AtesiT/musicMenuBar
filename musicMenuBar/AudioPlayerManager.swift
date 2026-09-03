7d1a1292700aec50cbf88c8f0c33fbab61db912f
AtesiT <atesit@mail.ru>
2026-09-03 13:42:28 +0300

    ADD: functions to manage a playlist

--- musicMenuBar/AudioPlayerManager.swift
+++ musicMenuBar/AudioPlayerManager.swift
@@ -6,24 +6,38 @@ import Observation
 final class AudioPlayerManager: NSObject, AVAudioPlayerDelegate {
 
     private(set) var isPlaying: Bool = false
-    private(set) var currentTrack: Track?
     private(set) var currentTime: TimeInterval = 0
     private(set) var duration: TimeInterval = 0
+    private(set) var playlist = Playlist()
+
+    var currentTrack: Track? { playlist.currentTrack }
 
     private var player: AVAudioPlayer?
     private var progressTimer: Timer?
 
-    func loadTrack(from url: URL) {
-        stop()
+    private let lastTrackBookmarkKey = "lastTrackBookmark"
 
-        guard let player = try? AVAudioPlayer(contentsOf: url) else { return }
-        player.delegate = self
-        player.prepareToPlay()
+    override init() {
+        super.init()
+        restoreLastTrack()
+    }
 
-        self.player = player
-        currentTrack = Track(title: url.deletingPathExtension().lastPathComponent, url: url)
-        duration = player.duration
-        currentTime = 0
+    func loadFiles(from urls: [URL]) {
+        let newTracks = urls.map { Track(title: $0.deletingPathExtension().lastPathComponent, url: $0) }
+        let shouldAutoload = playlist.currentTrack == nil
+
+        playlist.append(newTracks)
+
+        if shouldAutoload, let first = playlist.currentTrack {
+            load(track: first)
+        }
+    }
+
+    func play(trackWithId id: Track.ID) {
+        playlist.selectTrack(with: id)
+        guard let track = playlist.currentTrack else { return }
+        load(track: track)
+        play()
     }
 
     func togglePlayback() {
@@ -43,12 +57,38 @@ final class AudioPlayerManager: NSObject, AVAudioPlayerDelegate {
         stopProgressTimer()
     }
 
+    func next() {
+        guard let track = playlist.next() else { return }
+        load(track: track)
+        play()
+    }
+
+    func previous() {
+        guard let track = playlist.previous() else { return }
+        load(track: track)
+        play()
+    }
+
     func seek(to time: TimeInterval) {
         player?.currentTime = time
         currentTime = time
     }
 
-    private func stop() {
+    private func load(track: Track) {
+        stopPlayback()
+
+        guard let player = try? AVAudioPlayer(contentsOf: track.url) else { return }
+        player.delegate = self
+        player.prepareToPlay()
+
+        self.player = player
+        duration = player.duration
+        currentTime = 0
+
+        saveLastTrackBookmark(for: track.url)
+    }
+
+    private func stopPlayback() {
         player?.stop()
         player = nil
         isPlaying = false
@@ -71,8 +111,26 @@ final class AudioPlayerManager: NSObject, AVAudioPlayerDelegate {
     }
 
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        isPlaying = false
-        currentTime = 0
-        stopProgressTimer()
+        next()
+    }
+
+    private func saveLastTrackBookmark(for url: URL) {
+        guard let bookmark = try? url.bookmarkData(options: .withSecurityScope) else { return }
+        UserDefaults.standard.set(bookmark, forKey: lastTrackBookmarkKey)
+    }
+
+    private func restoreLastTrack() {
+        guard let bookmark = UserDefaults.standard.data(forKey: lastTrackBookmarkKey) else { return }
+
+        var isStale = false
+        guard let url = try? URL(
+            resolvingBookmarkData: bookmark,
+            options: .withSecurityScope,
+            relativeTo: nil,
+            bookmarkDataIsStale: &isStale
+        ) else { return }
+
+        guard url.startAccessingSecurityScopedResource() else { return }
+        loadFiles(from: [url])
     }
 }
